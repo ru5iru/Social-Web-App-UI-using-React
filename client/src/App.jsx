732c42ef825aed5a265a394f3f4d880414297204
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-07-01 12:12:21 +0530

    login and register done

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Register from "./pages/register/Register";
+
+function App() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./register.scss"
+
+const Register = () => {
+  return (
+    <div className='register'>
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, erant dicunt eu eam, qui munere graecis suavitate ad. Ne paulo errem omittantur sit, vituperata accommodare pri ei.
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form action="">
+            <input type="text" placeholder='Username' />
+            <input type="email" placeholder='Email' />
+            <input type="password" placeholder='Password' />
+            <input type="text" placeholder='Name' />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
