fe913becb43bebdd8d0e1003859c1602444af51e
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-07-03 19:39:21 +0530

    dark mode done

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./register.scss"
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
@@ -11,7 +12,9 @@ const Register = () => {
             Lorem ipsum dolor sit amet, erant dicunt eu eam, qui munere graecis suavitate ad. Ne paulo errem omittantur sit, vituperata accommodare pri ei.
           </p>
           <span>Do you have an account?</span>
-          <button>Login</button>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
         </div>
         <div className="right">
           <h1>Register</h1>
