99f7ec63f3c39744a1ac75956cb4011420e6d9bb
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-07-03 22:24:21 +0530

    posts done

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./home.scss"
+import Stories from '../../components/stories/Stories'
+import Posts from '../../components/posts/Posts'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Stories />
+      <Posts />
+    </div>
   )
 }
 
