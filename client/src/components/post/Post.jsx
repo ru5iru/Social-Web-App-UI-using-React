e5d77bc1c63291cf8a8acd0750f15756478225a4
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-07-05 02:11:20 +0530

    design done

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -5,53 +5,62 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
+import Comments from "../comments/Comments";
+import { useState } from "react";
 
 const Post = ({ post }) => {
 
-    const liked = false;
+   const [commentOpen, setCommentOpen] = useState(false)
 
-    return (
-        <div className="post">
-            <div className="container">
-                <div className="user">
-                    <div className="userInfo">
-                        <img src={post.profilePic} alt="" />
-                        <div className="details">
-                            <Link
-                                to={`/profile/${post.userId}`}
-                                style={{
-                                    textDecoration: "none",
-                                    color: "inherit",
-                                }}
-                            >
-                                <span className="name">{post.name}</span>
-                            </Link>
-                            <span className="date">1 min ago</span>
-                        </div>
-                    </div>
-                    <MoreHorizIcon />
-                </div>
-                <div className="content">
-                    <p>{post.desc}</p>
-                    <img src={post.img} alt="" />
-                </div>
-                <div className="info">
-                    <div className="item">
-                        {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-                        12 Likes
-                    </div>
-                    <div className="item">
-                        <TextsmsOutlinedIcon />
-                        12 Comments
-                    </div>
-                    <div className="item">
-                        <ShareOutlinedIcon />
-                        Share
-                    </div>
-                </div>
+   const liked = false;
+
+   return (
+      <div className="post">
+         <div className="container">
+            <div className="user">
+               <div className="userInfo">
+                  <img src={post.profilePic} alt="" />
+                  <div className="details">
+                     <Link
+                        to={`/profile/${post.userId}`}
+                        style={{
+                           textDecoration: "none",
+                           color: "inherit",
+                        }}
+                     >
+                        <span className="name">{post.name}</span>
+                     </Link>
+                     <span className="date">1 min ago</span>
+                  </div>
+               </div>
+               <MoreHorizIcon />
             </div>
-        </div>
-    );
+            <div className="content">
+               <p>{post.desc}</p>
+               <img src={post.img} alt="" />
+            </div>
+            <div className="info">
+               <div className="item">
+                  {liked ? (
+                     <FavoriteOutlinedIcon />
+                  ) : (
+                     <FavoriteBorderOutlinedIcon />
+                  )}
+                  12 Likes
+               </div>
+               <div className="item" onClick={()=>setCommentOpen(!commentOpen)}>
+                  <TextsmsOutlinedIcon />
+                  12 Comments
+               </div>
+               <div className="item">
+                  <ShareOutlinedIcon />
+                  Share
+               </div>
+            </div>
+            {commentOpen && <Comments />}
+         </div>
+      </div>
+   );
 };
 
 export default Post;
